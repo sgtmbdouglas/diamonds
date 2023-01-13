f0e5e5ba27333196b19de2b846181e405ef8b116
DCBB <dc@pop-os.localdomain>
2023-01-13 17:14:29 -0300

    iniciando projeto configurado

---
+++ Shape/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Form;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var diamond = new Diamond();
+        Console.WriteLine("Bem vindo(a) ao Formas, começemos com um diamante!");
+        diamond.Form();
+    }
+    // public static void DiamondF(Diamond form)
+    // {
+    //     Console.WriteLine($" Exibindo {form.Name}");
+    //     Diamond.Form();
+    // }
+    
+}
